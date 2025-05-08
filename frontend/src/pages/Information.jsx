f55e3ca700c6af40fcbd0325647d96cbf45440c7
Dylan66 <dylannganga@gmail.com>
2025-05-08 18:09:15 +0300

    info

--- frontend/src/pages/Information.jsx
+++ frontend/src/pages/Information.jsx
@@ -35,13 +35,13 @@ const Information = () => {
         <img src={endomet2Image} alt="Endometriosis awareness" className={styles.heroImage} />
         <div className={styles.heroContent}>
           <h1 className={styles.heroHeading}>
-            Empowering Women through Menstrual Health Awareness
+          Finding the Right Menstrual Products
           </h1>
           <p className={styles.heroParagraph}>
-            Our mission is to raise awareness about menstrual health and
-            empower women with the knowledge and resources they need. We
-            envision a world where every woman has access to safe and hygienic
-            menstrual products.
+          Navigating the world of pads, tampons, cups, and more can be overwhelming! 
+            This article breaks down the different choices available, discussing pros, 
+            cons, and how to find the best fit for your body and lifestyle. 
+            Make informed choices for comfort and confidence every month.
           </p>
           <button className={styles.learnMoreButton} onClick={handleLearnMoreClick}>Learn More</button>
         </div>
