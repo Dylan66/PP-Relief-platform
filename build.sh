2a8e7aca6e4158f9a5f4a08dc1f2593194b3bf61
Dylan66 <dylannganga@gmail.com>
2025-05-10 11:01:24 +0300

    deploy instructions

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
+
+# Create superuser if environment variables are set
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_EMAIL}" && -n "${DJANGO_SUPERUSER_PASSWORD}" ]]; then
+  python manage.py createsuperuser --noinput
+fi 
